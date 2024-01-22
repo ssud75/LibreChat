3acabd3d8d752439b448e75700c9050a37df875c
Danny Avila <messagedaniel@protonmail.com>
2024-01-22 09:25:14 -0500

    feat: assistant icons in assistant switcher

--- client/src/components/Chat/Landing.tsx
+++ client/src/components/Chat/Landing.tsx
@@ -31,7 +31,7 @@ export default function Landing({ Header }: { Header?: ReactNode }) {
       <div className="absolute left-0 right-0">{Header && Header}</div>
       <div className="flex h-full flex-col items-center justify-center">
         <div className="mb-3 h-[72px] w-[72px]">
-          <div className="gizmo-shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black">
+          <div className="shadow-stroke relative flex h-full items-center justify-center rounded-full bg-white text-black">
             {endpoint &&
               Icon &&
               Icon({
