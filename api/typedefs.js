a670ab630b0b7bcfb3e10ca1d8e82f27d7fb0455
Danny Avila <messagedaniel@protonmail.com>
2024-01-28 16:38:11 -0500

    WIP: working first pass of toolCalls generated from openapi specs

--- api/typedefs.js
+++ api/typedefs.js
@@ -69,6 +69,12 @@
  */
 
 /**
+ * @exports ActionRequest
+ * @typedef {import('librechat-data-provider').ActionRequest} ActionRequest
+ * @memberof typedefs
+ */
+
+/**
  * @exports Action
  * @typedef {import('librechat-data-provider').Action} Action
  * @memberof typedefs
